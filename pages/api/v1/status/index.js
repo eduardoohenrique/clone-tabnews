378e9da73bc533c3a07c7025faa5b78b84302cf1
eduardoohenrique <eduardoohenriquej@gmail.com>
2025-03-24 16:59:25 +0000

    add `jsconfig.json` file

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,9 +1,14 @@
-import database from "../../../../infra/database.js";
+import database from "infra/database.js";
 
 async function status(request, response) {
-  const result = await database.query("SELECT 1 + 1 as sum;");
-  console.log(result.rows);
-  response.status(200).json({ chave: "são acima da média" });
+  try {
+    const result = await database.query("SELECT 1 + 1 as sum;");
+    console.log(result.rows);
+    response.status(200).json({ chave: "são acima da média" });
+  } catch (error) {
+    console.error("Database query failed:", error);
+    response.status(500).json({ error: "Internal Server Error" });
+  }
 }
 
 export default status;
